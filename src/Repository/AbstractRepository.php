286afdaf6bcaf769508b414659679bf0c26b6c35
Pedro Ribeiro da Silva Neto <pcelta.ribeiro@gmail.com>
2024-08-06 23:21:55 +1000

    feat: add unit tests for CurrencyRepository

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -8,9 +8,7 @@ class AbstractRepository
 {
     protected bool $isFlushDisabled = false;
 
-    public function __construct(protected EntityManagerInterface $em) {
-        $this->em = $em;
-    }
+    public function __construct(protected EntityManagerInterface $em) {}
 
     public function setIsFlushDisabled(bool $isFlushDisabled): void
     {
